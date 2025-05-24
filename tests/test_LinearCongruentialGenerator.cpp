ffb5badfe3f87d4b942346c66870677f74e55539
Eric-Butcher <ebutcher66@gmail.com>
2025-05-24 15:49:51 -0400

    Added simple test case for LCG contructor.

--- tests/test_LinearCongruentialGenerator.cpp
+++ tests/test_LinearCongruentialGenerator.cpp
@@ -1,8 +1,8 @@
 #include "gtest/gtest.h"
 #include "prng.hpp"
 #include "linear_congruential_generator.hpp"
-#include "xorshift.hpp"
 
-TEST(ArithmeticTest, OnePlusOneEqualsTwo) {
-    ASSERT_EQ(1+1, 2) << "1+1 is 2";
+// Test that the lcg constructors do not fail
+TEST(TestLinearCongruentialGenerator, Constructor) {
+    EXPECT_NO_THROW(LinearCongruentialGenerator());
 }
