d85b70d612991ad70769a6871dbaee91a8b1a9fd
Eric-Butcher <ebutcher66@gmail.com>
2025-05-24 17:20:18 -0400

    Added test cases for the lcg's generateUnitNormalRangeValu()

--- tests/test_LinearCongruentialGenerator.cpp
+++ tests/test_LinearCongruentialGenerator.cpp
@@ -31,3 +31,33 @@ TEST(TestLinearCongruentialGenerator, SeedAndCustomConstructor) {
     EXPECT_NO_THROW(LinearCongruentialGenerator(std::numeric_limits<uint64_t>::max() / 2, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF));
     EXPECT_NO_THROW(LinearCongruentialGenerator(std::numeric_limits<uint64_t>::max() - 1, 1, std::numeric_limits<uint64_t>::max() / 2, 58678575, 0xFFFFFF));
 }
+
+TEST(TestLinearCongruentialGenerator, GenerateUnitNormalRandomValueHasCorrectRange) {
+    LinearCongruentialGenerator lcg = LinearCongruentialGenerator();
+
+    short enough_iterations_to_be_confident = 1000;
+    for (short i = 0; i < enough_iterations_to_be_confident; ++i) {
+        double value = lcg.generateUnitNormalRandomValue();
+        EXPECT_GE(value, 0.0);
+        EXPECT_LE(value, 1.0);
+    }
+}
+
+
+/* This test of course is subject to some randomness 
+it is possible to fail even if everything is working 
+properly, but it would be incredibly unlikely to fail. 
+If this test ever fails it should be investigated further 
+manually. */ 
+TEST(TestLinearCongruentialGenerator, GenerateUnitNormalRandomValueAverageConvergesToExpected) {
+    LinearCongruentialGenerator lcg = LinearCongruentialGenerator();
+    double expected_average = 0.5;
+    double sum = 0.0;
+    short enough_iterations_to_be_confident = 10000;
+    for (short i = 0; i < enough_iterations_to_be_confident; ++i) {
+        sum += lcg.generateUnitNormalRandomValue();
+    }
+    double average = sum / enough_iterations_to_be_confident;
+    double confidence_interval = 1 / (enough_iterations_to_be_confident / 10);
+    EXPECT_NEAR(average, expected_average, confidence_interval);
+}
