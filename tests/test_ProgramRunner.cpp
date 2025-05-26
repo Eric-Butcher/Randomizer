e24f165019bbb367b7be6531ca9cdb1b7431a2c4
Eric-Butcher <ebutcher66@gmail.com>
2025-05-25 21:29:49 -0400

    Test declarations for ProgramRunner

---
+++ tests/test_ProgramRunner.cpp
@@ -0,0 +1,132 @@
+#include <gtest/gtest.h>
+#include "program_runner.hpp"
+
+TEST(TestProgramRunner, NoArgs){
+
+}
+
+TEST(TestProgramRunner, JustHelp){
+
+}
+
+TEST(TestProgramRunner, JustVersion){
+
+}
+
+TEST(TestProgramRunner, HelpThenVersion){
+
+}
+
+TEST(TestProgramRunner, VersionThenHelp){
+
+}
+
+TEST(TestProgramRunner, HelpThenOther){
+
+}
+
+TEST(TestProgramRunner, VersionThenOther){
+
+}
+
+TEST(TestProgramRunner, BadSubcommands){
+
+}
+
+TEST(TestProgramRunner, DefaultAlgorithm){
+
+}
+
+TEST(TestProgramRunner, ExplicitAlgorithmXOR){
+
+}
+
+TEST(TestProgramRunner, ExplicitAlgorithmLCG){
+
+}
+
+TEST(TestProgramRunner, ExplicitAlgorithmMersenne){
+
+}
+
+TEST(TestProgramRunner, ExplicitAlgorithmInvalid){
+
+}
+
+TEST(TestProgramRunner, NoMinOrMax){
+
+}
+
+TEST(TestProgramRunner, MinNoMax){
+
+}
+
+TEST(TestProgramRunner, MaxNoMin){
+
+}
+
+TEST(TestProgramRunner, MinAndMax){
+
+}
+
+TEST(TestProgramRunner, MinExcessiveNegativeMagnitude){
+
+}
+
+TEST(TestProgramRunner, MinExcessivePositiveMagnitude){
+
+}
+
+TEST(TestProgramRunner, MinAndMaxExcessiveNegativeMagnitude){
+
+}
+
+TEST(TestProgramRunner, MinAndMaxExcessivePositiveMagnitude){
+
+}
+
+TEST(TestProgramRunner, DefaultCount){
+
+}
+
+TEST(TestProgramRunner, ExplicitCount){
+
+}
+
+TEST(TestProgramRunner, CountIsZero){
+
+}
+
+TEST(TestProgramRunner, CountIsOne){
+
+}
+
+TEST(TestProgramRunner, CountIsNegative){
+
+}
+
+TEST(TestProgramRunner, CountExcessivePositiveMagnitude){
+
+}
+
+TEST(TestProgramRunner, DefaultType){
+
+}
+
+TEST(TestProgramRunner, ExplicitTypeUnit){
+
+}
+
+TEST(TestProgramRunner, ExplicitTypeFloat){
+
+}
+
+TEST(TestProgramRunner, ExplicitTypeInt){
+
+}
+
+TEST(TestProgramRunner, ExplicitTypeInvalid){
+
+}
+
+
