ed9777f05e4b624ed3cb355222a9b68f5fc4af4f
Eric-Butcher <ebutcher66@gmail.com>
2025-06-01 12:00:33 -0400

    Made it so that main() prints out status.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,18 @@
 
 int main(int argc, char *argv[]){
     ProgramRunner runner = ProgramRunner(argc, argv);
-    ProgramRunner::ProgramStatus status = runner.run();
-    return 0;
+    ProgramRunner::ProgramStatus status;
+    while (!runner.is_finished()) {
+        status = runner.iterate();
+        if (status.stdout_message.has_value()) {
+            std::cout << status.stdout_message.value() << std::endl;
+        }
+        if (status.stderr_message.has_value()) {
+            std::cerr << status.stderr_message.value() << std::endl;
+        }
+        if (status.exit_code.has_value()) {
+            exit(status.exit_code.value());
+        }
+    }
+    exit(1); // If we reach here, something went wrong
 }
